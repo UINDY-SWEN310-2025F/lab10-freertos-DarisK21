5a4e2a6cad29f37e1e6c24f92502a2ad29544a3e
Daris Kadric <kadricd@uindy.edu>
2025-11-14 10:11:02 -0500

    Lab 10 Daris

--- answer/answer.md
+++ answer/answer.md
@@ -1,1 +1,7 @@
 # Put your answer below
+
+ Part1-1a -  It uses 2  CPU Cores for my program 
+ Part1-1b - 40 MHz This is the default SPI Speed It was hard for me to find this on mine biut this is the defeault speed
+ Part1-1c - cpu freq: 160000000 Hz so its 160 MHz 
+ Part1-1d - 4 MB That is the default flash size 
+ Part1-1e - Minimum free heap size: 301164 bytes 
---
+++ answer/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+
+void taskFirst(void *p) {
+    int count = 0;
+    for (;;) {
+        printf("hello task1: count %d\n", count++);
+        vTaskDelay(500 / portTICK_PERIOD_MS);  
+    }
+}
+
+void taskSecond(void *p) {
+    int count = 0;
+    for (;;) {
+        printf("hello task2: count %d\n", count++);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+}
+
+void taskThird(void *p) {
+    int count = 0;
+    for (;;) {
+        printf("hello task3: count %d\n", count++);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+}
+
+void app_main(void) {
+    xTaskCreate(taskFirst,  "Task1", 2048, NULL, 1, NULL);  
+    xTaskCreate(taskSecond, "Task2", 2048, NULL, 2, NULL);  
+    xTaskCreate(taskThird,  "Task3", 2048, NULL, 3, NULL);  
+}
